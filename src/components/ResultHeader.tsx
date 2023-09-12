53b0cde834e74aac9e8220fad3041595b02f662d
Vincenzo Cristiano <viulenzgg@gmail.com>
2023-09-12 08:57:24 +0200

    removed play button when there is no audio file

--- src/components/ResultHeader.tsx
+++ src/components/ResultHeader.tsx
@@ -58,18 +58,20 @@ export const ResultHeader: React.FC<ResultHeaderProps> = ({
         <PhoneticTranscription>{phonetic}</PhoneticTranscription>
       </div>
 
-      <SVGStyled
-        xmlns="http://www.w3.org/2000/svg"
-        width="48"
-        height="48"
-        viewBox="0 0 75 75"
-        onClick={playAudio}
-      >
-        <g fill="#A445ED" fillRule="evenodd">
-          <circle cx="37.5" cy="37.5" r="37.5" opacity=".25" />
-          <path d="M29 27v21l21-10.5z" />
-        </g>
-      </SVGStyled>
+      {audio && (
+        <SVGStyled
+          xmlns="http://www.w3.org/2000/svg"
+          width="48"
+          height="48"
+          viewBox="0 0 75 75"
+          onClick={playAudio}
+        >
+          <g fill="#A445ED" fillRule="evenodd">
+            <circle cx="37.5" cy="37.5" r="37.5" opacity=".25" />
+            <path d="M29 27v21l21-10.5z" />
+          </g>
+        </SVGStyled>
+      )}
     </FlexContainer>
   )
 }
