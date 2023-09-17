f4b74d7d584e0ecd8991ea498b32656c9d4e4233
Vincenzo Cristiano <viulenzgg@gmail.com>
2023-09-17 17:33:43 +0200

    added spinner

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import { Navbar } from './Navbar'
 import { ResultHeader } from './ResultHeader'
 import { ErrorMsg } from './ErrorMsg'
+import { LoadingSpinner } from './LoadingSpinner'
 
-export { Navbar, ResultHeader, ErrorMsg }
+export { Navbar, ResultHeader, ErrorMsg, LoadingSpinner }
